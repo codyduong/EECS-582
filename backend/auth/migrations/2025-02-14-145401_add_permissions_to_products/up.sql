f16a343b3203fe3e5a5744e9622e4c8c9227de70
hvwendt <hvwendt@gmail.com>
2025-02-14 09:31:24 -0600

    SQL Migration for marketplace, product, price report
    up and down.sql and add to permission.rs to match the new migration

---
+++ backend/auth/migrations/2025-02-14-145401_add_permissions_to_products/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+INSERT INTO permissions (name) VALUES
+    ('create:product'), ('read:product'), ('update:product'), ('delete:product'),
+    ('create:marketplace'), ('read:marketplace'), ('update:marketplace'), ('delete:marketplace'),
+    ('create:price_report'), ('read:price_report'), ('update:price_report'), ('delete:price_report');
+
