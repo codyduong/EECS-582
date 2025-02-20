9f955f9580a951e92c97c987b577e50e45567a99
Cody Duong <duongcody507@gmail.com>
2025-02-20 00:57:38 -0600

    init frontend

---
+++ frontend/web/app/login/page.tsx
@@ -0,0 +1,12 @@
+/*
+ *  Page at "/login"
+ *
+ *  Authors: @codyduong
+ *  Date Created: 2025-02-19
+ *  Revision History:
+ *  - 2025-02-05 - @codyduong - initial creation of website
+ */
+
+export default function Login(): React.JSX.Element {
+  return <div>Login Page</div>;
+}
